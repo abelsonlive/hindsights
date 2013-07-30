d8fcf068a700d571bb85b28e26d99f1ba2686270
Brian Abelson <brianabelson@gmail.com>
2013-07-30 18:46:34 -0400

    s3 connection now tries 10 times before failing

--- s3.py
+++ s3.py
@@ -3,9 +3,12 @@
 import os
 import boto
 import boto.s3
+from boto.s3 import S3Connection
 from boto.s3.key import Key
 import sys
 import yaml
+from time import sleep
+from random import choice
 
 c = yaml.safe_load(open('config.yml'))
 
@@ -14,9 +17,27 @@ def find_s3_bucket(s3_conn, string):
         if string in i.name:
             return i
 
+def safe_connect():
+  try:
+    conn = boto.connect_s3(c['aws_access_key_id'], c['aws_secret_access_key'])
+  except:
+    do_try = True
+    i = 1
+    while do_try:
+      try:
+        conn = boto.connect_s3(c['aws_access_key_id'], c['aws_secret_access_key'])
+      except:
+        sleep(choice([float(i)/100 for i in range(500,1000)]))
+        i += 1
+        if i > 10:
+          do_try = False
+      else:
+        do_try = False
+  return conn
+
 def connect_to_bucket(bucket_name=c['s3_bucket']):
-	conn = boto.connect_s3(c['aws_access_key_id'], c['aws_secret_access_key'])
-	return find_s3_bucket(conn, bucket_name)
+  conn = safe_connect()
+  return find_s3_bucket(conn, bucket_name)
 
 def upload_string(string, filepath, bucket):
 	k = Key(bucket)
