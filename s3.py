d5a2b78c1a966f3f1982922da076b6d8876fb392
Brian Abelson <brianabelson@gmail.com>
2013-08-01 11:50:31 -0400

    fixed stupid whitespace problem

--- s3.py
+++ s3.py
@@ -39,9 +39,9 @@ def connect_to_bucket(bucket_name=c['s3_bucket']):
   return find_s3_bucket(conn, bucket_name)
 
 def upload_string(string, filepath, bucket):
-	k = Key(bucket)
-	k.key = filepath
-	print "/\ %s" % (filepath)
+  k = Key(bucket)
+  k.key = filepath
+  print "/\ %s" % (filepath)
   try:
     k.set_contents_from_string(string)
   except:
